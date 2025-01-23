6eb5e955d362ff7237755b2bc1159e7c5e1a9d8a
Kauan Monteiro Valentini <kauanfghjklpoi@gmail.com>
2025-01-23 01:54:12 -0300

    Sorteador de numeros completo

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -4,15 +4,41 @@ function sortear(){
     let ateNumero = parseInt(document.getElementById("ate").value);
     let numerosSortiados = [] ;
 
-    for (let i = 0; i < quantidadeDeNumeros; i++){
-        let numeroEscolhido = sorteadorDeNumeros(doNumero, ateNumero);  
-        numerosSortiados.push(numeroEscolhido);
+    adicionaNumerosSorteados();
+    mensagemFinal(numerosSortiados);
+    document.getElementById("btn-reiniciar").classList.add("container__botao");
+    document.getElementById("btn-reiniciar").classList.remove("container__botao-desabilitado");
+    
+    function adicionaNumerosSorteados() {
+        while(numerosSortiados.length < quantidadeDeNumeros){
+           let numeroEscolhido = sorteadorDeNumeros(doNumero, ateNumero);
+           if (numerosSortiados.includes(numeroEscolhido)) {
+               adicionaNumerosSorteados();
+           } else {
+               numerosSortiados.push(numeroEscolhido);
+           }
+       }
     }
+    
+}
 
-    console.log(numerosSortiados);
+function reiniciar(){
+    let mensagemFinal = document.getElementById("resultado");
+    document.getElementById("quantidade").value = "";
+    document.getElementById("de").value = "";
+    document.getElementById("ate").value = "";
+    document.getElementById("btn-reiniciar").classList.remove("container__botao");
+    document.getElementById("btn-reiniciar").classList.add("container__botao-desabilitado");
+    mensagemFinal.innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
 }
 
 function sorteadorDeNumeros(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  
+
+  function mensagemFinal(numerosSortiados){
+    if(numerosSortiados != 0){
+        let mensagemFinal = document.getElementById("resultado");
+        mensagemFinal.innerHTML = `<label class="texto__paragrafo">Os numeros sortiados são: ${numerosSortiados}</label>`;
+  }
+}
