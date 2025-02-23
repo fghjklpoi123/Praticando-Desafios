7310120b30c28fa96f14e951d4659f0d6d302c28
Kauan Monteiro Valentini <kauanfghjklpoi@gmail.com>
2025-02-23 02:53:02 -0300

    Consertando bugs

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -2,7 +2,13 @@ function sortear(){
     let quantidadeDeNumeros = parseInt(document.getElementById("quantidade").value);
     let doNumero = parseInt(document.getElementById("de").value);
     let ateNumero = parseInt(document.getElementById("ate").value);
+    let intervaloDeNumeros = ateNumero - doNumero;
     let numerosSortiados = [] ;
+
+    if (doNumero >= ateNumero || quantidadeDeNumeros  > intervaloDeNumeros + 1) {
+        alert("Intervalo de numeros inválidos!! Verifique novamente os campos.");
+        limparCampos();
+    }else{
 
     adicionaNumerosSorteados();
     mensagemFinal(numerosSortiados);
@@ -19,17 +25,22 @@ function sortear(){
            }
        }
     }
+}
     
 }
 
 function reiniciar(){
     let mensagemFinal = document.getElementById("resultado");
-    document.getElementById("quantidade").value = "";
-    document.getElementById("de").value = "";
-    document.getElementById("ate").value = "";
+    limparCampos();
     document.getElementById("btn-reiniciar").classList.remove("container__botao");
     document.getElementById("btn-reiniciar").classList.add("container__botao-desabilitado");
     mensagemFinal.innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+}
+
+function limparCampos(){
+    document.getElementById("quantidade").value = "";
+    document.getElementById("de").value = "";
+    document.getElementById("ate").value = "";
 }
 
 function sorteadorDeNumeros(min, max) {
